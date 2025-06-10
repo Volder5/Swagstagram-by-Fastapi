22ba352d17893ea5285507907ef0232c20119f86
Vova Prykhodki <vovaprykhodki@Vovas-MacBook-Air.local>
2025-06-10 18:30:20 +0200

    feature(auth): Now you can see 'config.py' file and was create readme file

---
+++ app/config/config.py
@@ -0,0 +1,12 @@
+URLS = {
+    "login": "/auth/login",
+    "login_post": "/auth/login_post",
+    "register": "/auth/register",
+    "register_post": "/auth/register_post",
+    "recovery": "/auth/recovery",
+    "email-verification": "/auth/verification",
+    "email-verification_post": "/auth/verification_post",
+    
+}
+
+
